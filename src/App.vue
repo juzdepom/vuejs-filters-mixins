0119008ce2934a6414be86bcc1f7a5dbeee7960f
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-26 08:55:55 -0800

    creating a local filter

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase | to-lowercase }}</p>
+                <p>{{ text | toUppercase}}</p>
                 <hr>
                 <input v-model="filterText">
                 <ul>
@@ -23,6 +23,7 @@
                 filterText: ''
             }
         },
+        //LOCAL FILTER
         filters: {
             toUppercase(value) {
                 return value.toUpperCase();
